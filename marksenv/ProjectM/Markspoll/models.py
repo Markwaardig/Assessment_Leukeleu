9959814cbc0181a6887bd3cd12a12605102fc19c
Markwaardig <markklaarenbeek@gmail.com>
2014-10-15 10:00:27 +0200

    Changed models again. This is the exact code following the djangoproject.com/en/1.7/intro/tutorial01/# tutorial

--- marksenv/ProjectM/Markspoll/models.py
+++ marksenv/ProjectM/Markspoll/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def __unicode__(self):
-        return self.question
+
 
 class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice = models.CharField(max_length=200)
-    votes = models.IntegerField()
-    def __unicode__(self):
-        return self.choice
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
